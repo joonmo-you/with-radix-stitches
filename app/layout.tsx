c35161df0b5723767b694089bab2707d3e1aea12
Joonmo You <joonmo.you@gmail.com>
2023-01-08 21:01:55 +0900

    config: access to the CSS string by using the getCssText function

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,21 @@
 import "./globals.css";
+import Header from "./Header";
+import { getCssText } from "../src/styles";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
+      <head>
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+      </head>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
